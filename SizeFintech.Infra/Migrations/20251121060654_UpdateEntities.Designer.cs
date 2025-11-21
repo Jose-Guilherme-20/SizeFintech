09076fbd09ac1e0468657f20d4fe44b0cb35c220
Jose Guilherme <jgscosta20@gmail.com>
2025-11-21 08:48:47 -0300

    chore: update migrations.

--- SizeFintech.Infra/Migrations/20251121060654_UpdateEntities.Designer.cs
+++ SizeFintech.Infra/Migrations/20251121060654_UpdateEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SizeFintech.Infra.Context;
 
@@ -11,9 +12,11 @@ using SizeFintech.Infra.Context;
 namespace SizeFintech.Infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121060654_UpdateEntities")]
+    partial class UpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
