9538e851a5c90787322787ea86b3cb543ce7ddfe
Jose Guilherme <jgscosta20@gmail.com>
2025-11-20 22:36:52 -0300

    feat: mapeamento das tabelas de notas fiscais e empresas.

---
+++ SizeFintech.Infra/Mapping/CompanyMap.cs
@@ -0,0 +1,49 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SizeFintech.Domain.Models.Entities;
+
+namespace SizeFintech.Infra.Mapping
+{
+    public class CompanyMap : BaseMap<CompanyEntity>
+    {
+        public override void Configure(EntityTypeBuilder<CompanyEntity> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("Company");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Nome)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            builder.Property(e => e.Cnpj)
+                .HasColumnType("varchar(14)")
+                .IsRequired();
+
+            builder.Property(e => e.Faturamento)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(e => e.LimiteCredito)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(e => e.RamoId)
+                .IsRequired();
+
+            builder.HasMany(e => e.NotasFiscais)
+                .WithOne(e => e.Empresa)
+                .HasForeignKey(e => e.EmpresaId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(e => e.Carrinhos)
+               .WithOne(e => e.Empresa)
+               .HasForeignKey(e => e.EmpresaId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
